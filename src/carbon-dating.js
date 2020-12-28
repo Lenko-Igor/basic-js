edc9f171c346c6e479e3e469489ad6e8ce5b91ac
Igor_Lenko <mk2008@mail.ru>
2020-12-28 13:11:09 +0300

    refactor: add a statement

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,12 +1,12 @@
-const CustomError = require("../extensions/custom-error");
+//const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(str) {
   
-  throw new CustomError('Not implemented');
-  if(typeof str === 'string'  && +str%1 === 0 && +str !== 0){
+  //throw new CustomError('Not implemented');
+  if(typeof str === 'string'  && +str%1 === 0 && +str > 0 && +str < 15){
     return Math.ceil(Math.log(MODERN_ACTIVITY/+str)/(0.693 / HALF_LIFE_PERIOD));
   } else {
     return false;
