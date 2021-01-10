1dfd73509232fc196c17f9f775c1c84c66d9d746
Igor Lenko <mk2008@mail.ru>
2021-01-10 11:29:05 +0300

    fix: change cod in what-season

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(Array.isArray(arr)){
+      let result = [];
+    
+      arr.forEach((e,i) => {
+        if (arr[i+1] === '--discard-prev'){
+          e = false;
+          arr[i+1] = false;
+        } else if(e === '--discard-next'){
+          e = false;
+          arr[i+1] = false;
+        }else if(e === '--double-next' && arr[i+1]){
+          result.push(arr[i+1]);
+        } else if(e === '--double-prev' && arr[i-1]){
+          result.push(arr[i-1]);
+        } else if(e === '--double-next' || e === '--double-prev' || e === '--discard-prev'){
+          e = false;
+        } else if(e){
+          result.push(e);
+        }
+      })
+  
+      return result;      
+    } else {
+      throw new CustomError('Not implemented');
+  }
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,17 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-  try {
-    if(date === undefined || typeof date !== 'object' || Object.keys(date).length !== 0){ throw new CustomError('Unable to determine the time of year!'); }
+  if(date){
+    if(isNaN(date.getTime())){ 
+      throw new CustomError('Unable to determine the time of year!'); 
+    } else {
+      let month = date.getMonth();
 
-    let month = date.getMonth();
+      return (month >= 2 && month <= 4)? 'spring' :
+      (month > 4 && month <= 7)? 'summer' : 
+      (month > 7 && month <= 10)? 'autumn' : 'winter';
+    }
+  } else {
+    return 'Unable to determine the time of year!';
+  }
+}
+  // catch(err){
+  //   return (date)? err : err.message;
+  // }
+  // try {
+  //   if(date === undefined || typeof date !== 'object' || Object.keys(date).length !== 0){ throw new CustomError('Unable to determine the time of year!'); }
 
-    return (month >= 2 && month <= 4)? 'spring' :
-    (month > 4 && month <= 7)? 'summer' : 
-    (month > 7 && month <= 10)? 'autumn' : 'winter';
-  }
-  catch(err){
-    return (date)? err : err.message;
-  }
-};
+  //   let month = date.getMonth();
+
+  //   return (month >= 2 && month <= 4)? 'spring' :
+  //   (month > 4 && month <= 7)? 'summer' : 
+  //   (month > 7 && month <= 10)? 'autumn' : 'winter';
+  // }
+  // catch(err){
+  //   return (date)? err : err.message;
+  // }
+//};
 
