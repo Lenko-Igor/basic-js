735af36af3fb8fd0951dd300f45ba64663f668f9
Igor Lenko <mk2008@mail.ru>
2021-01-18 11:56:34 +0300

    init: task transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,29 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  if(Array.isArray(arr)){
-      let result = [];
+    if(!Array.isArray(arr)){
+        return error;
+      }else{
+        let result = [];
     
-      arr.forEach((e,i) => {
-        if (arr[i+1] === '--discard-prev'){
-          e = false;
-          arr[i+1] = false;
-        } else if(e === '--discard-next'){
-          e = false;
-          arr[i+1] = false;
-        }else if(e === '--double-next' && arr[i+1]){
-          result.push(arr[i+1]);
-        } else if(e === '--double-prev' && arr[i-1]){
-          result.push(arr[i-1]);
-        } else if(e === '--double-next' || e === '--double-prev' || e === '--discard-prev'){
-          e = false;
-        } else if(e){
-          result.push(e);
-        }
-      })
-  
-      return result;      
-    } else {
-      throw new CustomError('Not implemented');
-  }
+        arr.forEach((e,i) => {
+          if(e === '--double-next'){
+            result.push(arr[i+1]);
+          } else if(e === '--double-prev'){
+            result.push(result[result.length-1]);
+          } else if(arr[i+1] === '--discard-prev'){
+            result.push(undefined);
+          } else if(arr[i-1] === '--discard-next'){
+            result.push(undefined);
+          } else {
+            result.push(e);
+          }
+        })
+    
+        return result.filter(e => {
+            if(e !== undefined && e !== '--discard-prev' && e !== '--discard-next'){
+              return true;
+            }else{
+              return false;
+            }
+          });
+      }
+
 };
