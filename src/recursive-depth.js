bb28d165d40ef3bc52a6759ccc87f2610c373cb9
Igor Lenko <mk2008@mail.ru>
2021-01-19 11:40:35 +0300

    init: the task recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
+  calculateDepth(arr) {
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    let array = [];
+
+    arr.forEach(e => {
+      if(Array.isArray(e)){
+        if(e.length === 0){
+          e[0] = 1;
+          array = array.concat(e);  
+        }else{
+          array = array.concat(e);
+        }
+      }
+    })
+   
+    if(array.length === 0){
+      return 1;
+    } else {
+      return 1 + this.calculateDepth(array);
+    }
   }
 };
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  value : [],
   getLength() {
-    throw new CustomError('Not implemented');
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    return this.value.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    (value !== undefined)? this.value.push(value) : this.value.push(' ');
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    if(position%1 === 0 && position > 0 && typeof position !== 'string'){
+      this.value.splice(position - 1, 1);
+      return this;
+    } else {
+      return error;
+    }
+
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    this.value.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    let result = '';
+    this.value.forEach((e, i) => {
+      if(i === 0){
+        result = result + `( ${e} )`;
+      } else {
+        result = result + `~~( ${e} )`;
+      }
+
+    })
+    this.value = [];
+    return result;
   }
 };
 
