0a4d613754d76b3756fd754fd1cef135c2c842c6
Igor Lenko <mk2008@mail.ru>
2021-01-22 12:24:59 +0300

    inint: the task simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,50 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  value : [],
+  value: [],
   getLength() {
-    //throw new CustomError('Not implemented');
-    // remove line with error and write your code here
     return this.value.length;
   },
-  addLink(value) {
-    //throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-    (value !== undefined)? this.value.push(value) : this.value.push(' ');
+  addLink(v) {
+    arguments.length > 0 ? this.value.push(String(v)) : this.value.push(" ");
     return this;
   },
   removeLink(position) {
-    //throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-    if(position%1 === 0 && position > 0 && typeof position !== 'string'){
+    if (position % 1 === 0 && position > 0 && typeof position !== "string") {
       this.value.splice(position - 1, 1);
       return this;
     } else {
-      return error;
+      this.value = [];
+      throw new Error("Error");
     }
-
   },
   reverseChain() {
-    //throw new CustomError('Not implemented');
-    // remove line with error and write your code here
     this.value.reverse();
     return this;
   },
   finishChain() {
-    //throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-    let result = '';
+    let result = "";
     this.value.forEach((e, i) => {
-      if(i === 0){
+      if (i === 0) {
         result = result + `( ${e} )`;
       } else {
         result = result + `~~( ${e} )`;
       }
-
-    })
+    });
     this.value = [];
     return result;
-  }
+  },
 };
+
 
 module.exports = chainMaker;
